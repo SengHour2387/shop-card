ae089af67826bbc1abc032937afbf7f285fedc45
SengHour2387 <166191211+SengHour2387@users.noreply.github.com>
2024-05-27 16:35:24 +0700

    Add files via upload

--- web development/script.js
+++ web development/script.js
@@ -1,3 +1,9 @@
-function changing() {
-    document.getElementsByClassName("them").innerHTML = "Light";
+/*function openPanel() {
+    document.getElementById("panel").style.left = 0;
+*/
+function openPanel() {
+    document.documentElement.style.setProperty('--hidepanel' , '0')
+}
+function closePanel() {
+    document.documentElement.style.setProperty('--hidepanel' ,'-100%')
 }
